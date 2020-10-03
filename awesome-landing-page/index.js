e5274f7b20537ba25793649a73096a25e8b09b1d
Radovitto <72159288+Radovitto@users.noreply.github.com>
2020-10-03 15:56:16 +0400

    added awesom landing page JS improvements

---
+++ awesome-landing-page/index.js
@@ -0,0 +1,23 @@
+
+
+for(var i = 0 ; i < 3 ; i++) {
+   if(i % 2 == 0){}
+
+   alert("You will have to click 3 times to proceed bro! Get ready for some disappearing tho!")
+   console.log("I won't cheat on the exam.")
+}
+
+function greet(name){
+   console.log("Hello, " + name)
+}
+
+//$("#plant-a-tree").hide();
+$("#make-friends").fadeOut(500).delay(1000).fadeIn(500)
+
+$("#closeButton").click(function (){
+   $("#newsletter").slideToggle(500);
+});
+
+$(".card").hover(function (){
+   $(".card").css({"width": "200px", "font-size": "150%"})
+})
